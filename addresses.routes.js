849b1701e97173bccba0d4365739a6f82827686c
Jameel Matin <jameel.matin@gmail.com>
2017-03-16 10:25:36 -0700

    Update addresses.routes.js

--- addresses.routes.js
+++ addresses.routes.js
@@ -1,6 +1,5 @@
-// Create addresses route file that will create an instance of the router and direct requests to /api/addresses to correct addresses controller functions
-const router = require('express').Router() // node is providing the router library
-const addressesController = require('../controllers/addresses.controller')() // we require addressesController and invoke it.we get the object that gives us access to all of the function inside of our closure
+const router = require('express').Router() 
+const addressesController = require('../controllers/addresses.controller')() 
 const addressesValidation = require('../middleware/addresses.validation')
 const cache = require('../../config/apicache').cache.middleware
 module.exports = router
@@ -11,9 +10,8 @@ module.exports = router
 router.get('/', addressesController.getAll)
 router.get('/', cache(), addressesController.getAll)
 router.get('/:id', addressesController.getOneById)
-router.put('/:id', addressesValidation.validateAddressesParams, addressesController.updateById) // By doing this, we are telling express that the request needs to pass through these functions before continuing on to the controllers.
+router.put('/:id', addressesValidation.validateAddressesParams, addressesController.updateById) 
 router.delete('/:id', addressesController.removeById)
-router.post('/', addressesValidation.validateAddressesParams, addressesController.insert) // By doing this, we are telling express that the request needs to pass through these functions before continuing on to the controllers.
+router.post('/', addressesValidation.validateAddressesParams, addressesController.insert) 
 router.get('/get/:id', addressesController.getAddressesByUser)
 router.post('/create', addressesValidation.validateAddressesParams, addressesController.createUserAddress)
-// router.delete('/:id', addressesController.removeAddress) addressesValidation.validateAddressesParams - updateById, insert, create
