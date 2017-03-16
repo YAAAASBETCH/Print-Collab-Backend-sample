6798278703af2491b530507bbbb6d8784ad61a4c
Jameel Matin <jameel.matin@gmail.com>
2017-03-16 10:38:11 -0700

    Update addresses.validation.js

--- addresses.validation.js
+++ addresses.validation.js
@@ -1,8 +1,7 @@
-// we are exporting an object that has the function validateAddressesParams.
 const util = require('util')
 
 module.exports = {
-    validateAddressesParams // This function takes the request , response and the next function as parameters.  Using functionality that we gain from express-validator, we can then use .checkBody to check name parameter is not empty, i.e. required.
+    validateAddressesParams 
 }
 
 function validateAddressesParams(req, res, next) {
@@ -26,7 +25,3 @@ function validateAddressesParams(req, res, next) {
         next()
     })
 }
-// in this file, we have hooked in middleware function in both the post and put api routes. These middleware functions sit as the parameters before the controller functions.  By doing this, we are telling express that the request needs to pass through these functions before continuing on to the controllers.
-// Inside the hackerValidation.js, we are exporting an object that has the function validatehackerParams. This function takes the request , response and the next function as parameters.  Using functionality that we gain from express-validator, we can then use .checkBody to check name parameter is not empty, i.e. required.
-// After checking the parameters, you need to run .getValidationResult() which will asynchronously get the validation errors. Using the promise syntax, we then check if there are any errrors, and if so return a 400 response with those errors.
-// If there are no errors, we call the next() function which will continue the request and pass it to our controllers.
